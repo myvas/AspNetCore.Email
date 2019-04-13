f43cc4409beb3197046d482ec426c1a82e0bbd3f
FrankH <4848285@qq.com>
2019-04-13 17:05:14 +0800

    update

--- samples/EmailApi/Controllers/EmailController.cs
+++ samples/EmailApi/Controllers/EmailController.cs
@@ -20,9 +20,53 @@ namespace Myvas.AspNetCore.WebApi.EmailApi.Controllers
             _emailSender = emailSender ?? throw new ArgumentNullException(nameof(emailSender));
         }
 
+        [HttpGet("plain-sample")]
+        public IActionResult PlainEmailSample()
+        {
+            var result = new EmailDto()
+            {
+                Recipients = "4848285@qq.com;noreply@myvas.com",
+                Subject = "Test Myvas.AspNetCore.Email (Plain Text)",
+                Body = @"Hello, 
+
+This is a TEST email from WebApi.
+
+Best Regards,
+Myvas.AspNetCore.Email",
+            };
+
+            return Json(result);
+        }
+
+        [HttpGet("html-sample")]
+        public IActionResult HtmlEmailSample()
+        {
+            var result = new EmailDto()
+            {
+                Recipients = "4848285@qq.com;noreply@myvas.com",
+                Subject = "Test Myvas.AspNetCore.Email (HTML)",
+                Body = @"<h1>Hello, </h1>
+<p>This is a <strong>TEST</strong> email from WebApi.</p>
+<br />
+<p>Best Regards,</p>
+<p>Myvas.AspNetCore.Email</p>",
+                IsBodyHtml = true
+            };
+
+            return Json(result);
+        }
+
+
         /// <summary>
         /// Send an Email.
         /// </summary>
+        /// <remarks>
+        /// {
+        ///  "recipients": "4848285@qq.com;noreply@test.com",
+        ///  "subject": "Test Myvas.AspNetCore.Email", 
+        ///  "body": "This is a test email from Myvas.AspNetCore.Email/WebApi."
+        /// }
+        /// </remarks>
         /// <returns></returns>
         [HttpPost]
         public async Task<IActionResult> SendAsync([FromBody]EmailDto input)
